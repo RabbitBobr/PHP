6a322383477109c8055d472774b974c33b146086
Sergey <RabbitBobr@gmail.com>
2018-12-23 19:19:07 +0300

    product_test_parsing

--- toyShop/model/connect_to_bd.php
+++ toyShop/model/connect_to_bd.php
@@ -1,13 +1,13 @@
 <?php ## Обработка ошибки соединения с базой данных
 try {
     $pdo = new PDO(
-        'mysql:host=localhost;dbname=shopToy;charset=utf8',
-        'rabbik',
-        'Dtytnfhbq',
+        'mysql:host=109.95.210.219;dbname=u47513;charset=utf8',
+        'u47513_rabbik',
+        'Dtytnfhbq666',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 }
 catch (PDOException $e) {
     echo "Невозможно установить соединение с базой данных";
 }
-?>
+
